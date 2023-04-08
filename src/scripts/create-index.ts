07f18a95764e46d2cbe25fd7beca563ed5a7ee94
yukineko <27853966+hideki0403@users.noreply.github.com>
2023-04-09 03:04:59 +0900

    refactor: create-inedxのパフォーマンス改善

--- src/scripts/create-index.ts
+++ src/scripts/create-index.ts
@@ -4,32 +4,26 @@ import config from '@/config'
 import resolveFilename from '@/utils/resolve-filename'
 import * as database from '@/database/controller'
 
-export default function createIndex() {
+export default async function createIndex() {
     const objects = listFiles(config.storage.path)
     console.log(`Found ${objects.length} objects.`)
     console.log('Creating index...')
 
-    const index = objects.map(async filePath => {
+    for (const filePath of objects) {
         const path = filePath.replace(config.storage.path + '/', '')
         const [bucket, ...key] = path.split('/')
         const stat = await resolveFilename(null, filePath, null)
 
-        return {
+        database.putObject({
             bucket,
             key: key.join('/'),
             filename: stat.name,
             mime: stat.mime || 'application/octet-stream',
             size: fs.statSync(filePath).size,
-        }
-    })
-
-    Promise.all(index).then(index => {
-        index.forEach(object => {
-            database.putObject(object)
         })
-        
-        console.log('Index created.')
-    })
+    }
+
+    console.log('Index created.')
 }
 
 function listFiles(baseDir: string): string[] {
